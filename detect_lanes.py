3903026e3fb32bf2c009d2f1ab63784b72aed913
Pankaj Mishra <pmishra02138@gmail.com>
2017-11-13 23:17:33 -0800

    Highlight detected lanes in original image

--- detect_lanes.py
+++ detect_lanes.py
@@ -123,6 +123,30 @@ def plot_lanes(out_img, ploty, left_fitx, right_fitx):
     time.sleep(2)
     plt.close()
 
+def highlight_lanes(img, binary_warped, Minv, ploty, left_fitx, right_fitx):
+    # Create an image to draw the lines on
+    warp_zero = np.zeros_like(binary_warped).astype(np.uint8)
+    color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+
+    # Recast the x and y points into usable format for cv2.fillPoly()
+    pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+    pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+    pts = np.hstack((pts_left, pts_right))
+
+    # Draw the lane onto the warped blank image
+    cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
+
+    # Warp the blank back to original image space using inverse perspective matrix (Minv)
+    newwarp = cv2.warpPerspective(color_warp, Minv, (img.shape[1], img.shape[0]))
+    # Combine the result with the original image
+    result = cv2.addWeighted(img, 1, newwarp, 0.3, 0)
+    plt.imshow(cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
+
+    # Display every plto for 2 secs
+    plt.show(block=False)
+    time.sleep(2)
+    plt.close()
+
 if __name__ == '__main__':
 
     # Read in the saved camera matrix and distortion coefficients
@@ -137,15 +161,16 @@ if __name__ == '__main__':
         # Binary color transform
         color_binary, combined_binary = color_pipeline(img)
         # Undistort and apply perspective the image
-        binary_warped, perspective_M, src = corners_unwarp(combined_binary, cal_file)
+        binary_warped, perspective_M, Minv, src = corners_unwarp(combined_binary, cal_file)
 
         # Sliding window for detecting lanes
         out_img, ploty, left_fitx, right_fitx = sliding_window(binary_warped)
+        # Calulate radius of curvature
+        left_rad, right_rad = calculate_curvature(ploty, left_fitx, right_fitx)
+        # print(round(left_rad,2), 'km', round(right_rad, 2), 'km')
 
         # Visualize lanes and fitted polynomial
         plot_lanes(out_img, ploty, left_fitx, right_fitx)
 
-        # Calulate radius of curvature
-        left_rad, right_rad = calculate_curvature(ploty, left_fitx, right_fitx)
-
-        print(round(left_rad,2), 'km', round(right_rad, 2), 'km')
+        # Create an image to draw the lines on
+        highlight_lanes(img, binary_warped, Minv, ploty, left_fitx, right_fitx)
